c936701c54e79e056081ed010b30ef4384eb9c7b
ayox <a91913@gmail.com>
2019-06-20 13:54:40 +0800

    Fix loading indicators when changing pages

--- src/app/stories/stories.component.ts
+++ src/app/stories/stories.component.ts
@@ -10,8 +10,9 @@ import { ActivatedRoute } from "@angular/router";
 export class StoriesComponent implements OnInit {
   typeSub: any;
   pageSub: any;
-  items;
+  items = [];
   storiesType;
+  isLoading = true;
   pageNum: number;
   listStart: number;
   constructor(
@@ -23,16 +24,23 @@ export class StoriesComponent implements OnInit {
     this.typeSub = this.route.data.subscribe(
       data => (this.storiesType = (data as any).storiesType)
     );
-
     this.pageSub = this.route.params.subscribe(params => {
       this.pageNum = +params.page ? +params.page : 1;
-      this._hnApiService
-        .fetchStories(this.storiesType, this.pageNum)
-        .subscribe(
-          items => (this.items = items),
-          error => console.log("Error fetching" + this.storiesType + "stories"),
-          () => (this.listStart = (this.pageNum - 1) * 30 + 1)
-        );
+      this.isLoading = true;
+      this._hnApiService.fetchStories(this.storiesType, this.pageNum).subscribe(
+        items => {
+          this.isLoading = false;
+          this.items = items;
+        },
+        error => {
+          console.log("Error fetching" + this.storiesType + "stories");
+          this.isLoading = false;
+        },
+        () => {
+          this.listStart = (this.pageNum - 1) * 30 + 1;
+          this.isLoading = false;
+        }
+      );
     });
   }
 }
